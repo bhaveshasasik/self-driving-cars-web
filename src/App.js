575c400a067c0ecb69008270e357040da3fc3f3a
Bhavesha Sasikumar <bhaveshasasi1234@gmail.com>
2024-06-11 16:44:02 -0700

    pictures added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Global.css";
+
+export default function Home() {
+  return(
+    <main className="w-full h-full text-center">
+      <div className='bg-tesla-background bg-cover bg-center h-screen'>
+        <h1 className='text-black font-sans text-8xl'>ECS 188 Final</h1>
+      </div>
+      <div className='bg-garage-background bg-cover bg-center h-screen'>
+        <h1 className='text-black font-sans text-8xl'>ECS 188 Final</h1>
+      </div>
+      <div className='bg-forest-background bg-cover bg-center h-screen'>
+        <h1 className='text-black font-sans text-8xl'>ECS 188 Final</h1>
+      </div>
+      <div className='bg-self-background bg-cover bg-center h-screen'>
+        <h1 className='text-black font-sans text-8xl'>ECS 188 Final</h1>
+      </div>
+      <div className='bg-conclusion-background bg-cover bg-center h-screen'>
+        <h1 className='text-black font-sans text-8xl'>ECS 188 Final</h1>
+      </div>
+    </main>
+    
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'tesla-background': "url('/tesla_image.jpg')",
+        'self-background': "url('/image_1.jpg')",
+        'garage-background': "url('/image_2.jpg')",
+        'forest-background': "url('/image_3.jpg')",
+        'conclusion-background': "url('/image_4.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
